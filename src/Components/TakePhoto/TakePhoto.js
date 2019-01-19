bd54256184d825b7aec48e328d5013d213c2f850
SmileWuji <kai.wen@mail.utoronto.ca>
2019-01-19 15:31:23 -0500

    Google Cloud Vision api.

--- src/Components/TakePhoto/TakePhoto.js
+++ src/Components/TakePhoto/TakePhoto.js
@@ -11,43 +11,69 @@ class TakePhoto extends Component {
 
         };
     }
+    
     onCameraError (error) {
-        console.error('onCameraError', error);
-      }
-     
-      onCameraStart (stream) {
-          //
-        console.log('onCameraStart');
-      }
-     
-      onCameraStop () {
-        console.log('onCameraStop');
-      }
+      console.error('onCameraError', error);
+    }
+   
+    onCameraStart (stream) {
+        //
+      console.log('onCameraStart');
+    }
+   
+    onCameraStop () {
+      console.log('onCameraStop');
+    }
+    
     onTakePhoto (dataUri) {
-        // Send the data to API and then use Vision Cloud API to process the image prediction.
-        console.log('takePhoto');
-      }
-     
-      render () {
-        return (
-          <div className="TakePhoto">
-            <Camera
-              onTakePhoto = { (dataUri) => { this.onTakePhoto(dataUri); } }
-              onCameraError = { (error) => { this.onCameraError(error); } }
-              idealFacingMode = {FACING_MODES.ENVIRONMENT}
-              idealResolution = {{width: 640, height: 480}}
-              imageType = {IMAGE_TYPES.JPG}
-              imageCompression = {0.97}
-              isMaxResolution = {false}
-              isImageMirror = {false}
-              isDisplayStartCameraError = {true}
-              sizeFactor = {1}
-              onCameraStart = { (stream) => { this.onCameraStart(stream); } }
-              onCameraStop = { () => { this.onCameraStop(); } }
-            />
-          </div>
-        );
-      }
+      var image_content = dataUri.substring(dataUri.indexOf('base64,') + 7);
+      fetch('https://vision.googleapis.com/v1/images:annotate?key=AIzaSyDKektM6JoWOc5myBNz9rodeuYm58jbbVI', {
+        method: 'POST',
+        body: JSON.stringify({
+          "requests": [
+            {
+              "features": [
+                {
+                  "type": "WEB_DETECTION"
+                },
+                {
+                  "type": "LABEL_DETECTION"
+                }
+              ],
+              "image": {
+                "content": image_content
+              }
+            }
+          ]
+        })
+      }).then((res) => {
+        console.log(123)
+        return res.json()
+      }).then((data) => {
+        console.log(data)
+      });
+    }
+   
+    render () {
+      return (
+        <div className="TakePhoto">
+          <Camera
+            onTakePhoto = { (dataUri) => { this.onTakePhoto(dataUri); } }
+            onCameraError = { (error) => { this.onCameraError(error); } }
+            idealFacingMode = {FACING_MODES.ENVIRONMENT}
+            idealResolution = {{width: 640, height: 480}}
+            imageType = {IMAGE_TYPES.JPG}
+            imageCompression = {0.97}
+            isMaxResolution = {false}
+            isImageMirror = {false}
+            isDisplayStartCameraError = {true}
+            sizeFactor = {1}
+            onCameraStart = { (stream) => { this.onCameraStart(stream); } }
+            onCameraStop = { () => { this.onCameraStop(); } }
+          />
+        </div>
+      );
+    }
 }
      
     export default TakePhoto;
