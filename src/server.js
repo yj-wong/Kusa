3248e791219adcde1f914a0b88409c2187cc2cde
LeonYuAng3NT <leonzhang1996@hotmail.com>
2019-01-19 14:53:03 -0500

    change on server, config and index file

---
+++ src/server.js
@@ -0,0 +1,115 @@
+/**
+ * React Starter Kit (https://www.reactstarterkit.com/)
+ *
+ * Copyright © 2014-2016 Kriasoft, LLC. All rights reserved.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE.txt file in the root directory of this source tree.
+ */
+
+import 'babel-polyfill';
+import path from 'path';
+import express from 'express';
+
+import Router from './routes';
+import assets from './assets';
+import { port, auth, analytics } from './config';
+
+const server = global.server = express();
+
+//
+// Tell any CSS tooling (such as Material UI) to use all vendor prefixes if the
+// user agent is not known.
+// -----------------------------------------------------------------------------
+global.navigator = global.navigator || {};
+global.navigator.userAgent = global.navigator.userAgent || 'all';
+
+//
+// Register Node.js middleware
+// -----------------------------------------------------------------------------
+server.use(express.static(path.join(__dirname, 'public')));
+server.use(cookieParser());
+server.use(bodyParser.urlencoded({ extended: true }));
+server.use(bodyParser.json());
+
+//
+// Authentication
+// -----------------------------------------------------------------------------
+server.use(expressJwt({
+  secret: auth.jwt.secret,
+  credentialsRequired: false,
+  /* jscs:disable requireCamelCaseOrUpperCaseIdentifiers */
+  getToken: req => req.cookies.id_token,
+  /* jscs:enable requireCamelCaseOrUpperCaseIdentifiers */
+}));
+server.use(passport.initialize());
+
+
+//
+// Register API middleware
+// -----------------------------------------------------------------------------
+server.use('/graphql', expressGraphQL(req => ({
+  schema,
+  graphiql: true,
+  rootValue: { request: req },
+  pretty: process.env.NODE_ENV !== 'production',
+})));
+
+//
+// Register server-side rendering middleware
+// -----------------------------------------------------------------------------
+server.get('*', async (req, res, next) => {
+  try {
+    let statusCode = 200;
+    const template = require('./views/index.jade');
+    const data = { title: '', description: '', css: '', body: '', entry: assets.main.js };
+
+    if (process.env.NODE_ENV === 'production') {
+      data.trackingId = analytics.google.trackingId;
+    }
+
+    const css = [];
+    const context = {
+      insertCss: styles => css.push(styles._getCss()),
+      onSetTitle: value => (data.title = value),
+      onSetMeta: (key, value) => (data[key] = value),
+      onPageNotFound: () => (statusCode = 404),
+    };
+
+    await Router.dispatch({ path: req.path, query: req.query, context }, (state, component) => {
+      data.body = ReactDOM.renderToString(component);
+      data.css = css.join('');
+    });
+
+    res.status(statusCode);
+    res.send(template(data));
+  } catch (err) {
+    next(err);
+  }
+});
+
+//
+// Error handling
+// -----------------------------------------------------------------------------
+const pe = new PrettyError();
+pe.skipNodeFiles();
+pe.skipPackage('express');
+
+server.use((err, req, res, next) => { // eslint-disable-line no-unused-vars
+  console.log(pe.render(err)); // eslint-disable-line no-console
+  const template = require('./views/error.jade');
+  const statusCode = err.status || 500;
+  res.status(statusCode);
+  res.send(template({
+    message: err.message,
+    stack: process.env.NODE_ENV === 'production' ? '' : err.stack,
+  }));
+});
+
+//
+// Launch the server
+// -----------------------------------------------------------------------------
+server.listen(port, () => {
+  /* eslint-disable no-console */
+  console.log(`The server is running at http://localhost:${port}/`);
+});
