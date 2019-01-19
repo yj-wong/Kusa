4c45bd0d7fa33b9ace489c7d2a0e5756e7ad5753
yj-wong <yujuewong15@gmail.com>
2019-01-19 09:41:25 -0500

    UI

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import React, { Component } from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import Feedback from './Feedback';
 
 class App extends Component {
   toScan() {
-    alert("scan");
-  }
-
-  toFeedback() {
-
   }
 
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          Greetings
+          <p>Our daily habits may affect the environment in ways that we might not notice.</p>
+          <p>Start Scanning to learn more about your habits.</p>
         </header>
-        <div className="buttons">
-        <button onClick={this.toScan}>
-        Start Scanning
-        </button>
+        <div>
+          <button onClick={this.toScan}>Start Scanning</button>
+        </div>
+        <div>
+          <button onClick={this.toFeedback}>Get Feedback</button>
         </div>
       </div>
     );
