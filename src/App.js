103b7a440b6ea7ca9e22fc1c8224a582acdec2c9
LeonYuAng3NT <leonzhang1996@hotmail.com>
2019-01-19 11:31:43 -0500

    fixed not found components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
-import Feedback from './Feedback';
 
 class App extends Component {
   toScan() {
