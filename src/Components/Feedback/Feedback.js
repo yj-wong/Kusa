f7791a16d135156f6fc15f8ef4e0703fa04ed26a
yj-wong <yujuewong15@gmail.com>
2019-01-19 20:11:54 -0500

    Feedback Link

--- src/Components/Feedback/Feedback.js
+++ src/Components/Feedback/Feedback.js
@@ -4,41 +4,56 @@ import home from './Hotel-suite-living-room.jpg';
 import Score from '../Score/Score';
 
 class Feedback extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
+	constructor(props) {
+		super(props);
+		this.state = {
+			current_page: 'feedback_view',
 
-        items: [
-          { type: 'plastic bottles', score: 3, suggestion: "", id: 1 },
-          { type: 'running water faucet', score: 1, suggestion: "", id: 2 }
-        ]
-      }
-    }
+			items: [
+				{ type: 'plastic bottles', score: 3, suggestion: "", id: 1 },
+				{ type: 'running water faucet', score: 1, suggestion: "", id: 2 }
+			]
+		}
+		
+		this.toOverall = this.toOverall.bind(this);
+	}
 
-    getScore() {
+	toOverall() {
+		this.setState({current_page: 'score_view'})
+	}
 
-    }
+	render() {
+		let final_view = undefined    
+		if (this.state.current_page == 'feedback_view') {
+			final_view = (
+				<div className="Feedback">
+					<img src={home} alt="home" width="60%"/>
+					<header className="Feedback-header">
+						Items Recognized:
+					</header>
+					<div className="items"> 
+						<Items items={this.state.items}/>
+					</div>
+					<div>
+						<button onClick={this.toOverall}>Get Overall Score</button>
+					</div>
+					<div>
+						<button onClick={this.props.callBack}>Home</button>
+					</div>
+				</div>           
+			); 
+		} else if (this.state.current_page == 'score_view') {
+      final_view = (
+        <Score callBack={this.toHome} />
+			);
+		} 
+		else {
+ 		console.log('Something went wrong.')
+			final_view = <div />
+		}
 
-    
-    render() {
-        return (
-            <div className="Feedback">
-                <img src={home} alt="home" width="60%"/>
-                <header className="Feedback-header">
-                    Items Recognized:
-                </header>
-                <div className="items"> 
-                    <Items items={this.state.items}/>
-                </div>
-                <div>
-                    <button onClick={this.toOverall}>Get Overall Score</button>
-                </div>
-                <div>
-                    <button onClick={this.props.callBack}>Home</button>
-                </div>
-            </div>           
-        );
-    }
+		return final_view;
+	}
 }
 
 
@@ -51,7 +66,7 @@ class Feedback extends Component {
 //             <div>Amount: { item.amount }</div>
 //         </div>
 //     });
-            
+			
 //     return(
 //         <div className="item-list">
 //         { itemList }
@@ -61,32 +76,32 @@ class Feedback extends Component {
 
 class Items extends Component {
 
-    constructor(props){
-        super(props);
+	constructor(props){
+		super(props);
 
-    }
-    
+	}
+	
 
-    render() {
-        const items = this.props.items;
-        const itemList = items.map(item => {
-            return( 
-                <div className="item" key={item.id}>
-                <div>Type: { item.type }</div>
-                <div>Amount: { item.amount }</div>
-                </div>
-            )
-        })
-            
-        return(
-            <div className="item-list">
-            { itemList }
-            </div>
-        )
-        // return(
-        //     <p>there should be something</p>
-        //     )
-    }
+	render() {
+		const items = this.props.items;
+		const itemList = items.map(item => {
+			return( 
+				<div className="item" key={item.id}>
+				<div>Type: { item.type }</div>
+				<div>Amount: { item.amount }</div>
+				</div>
+			)
+		})
+			
+		return(
+			<div className="item-list">
+			{ itemList }
+			</div>
+		)
+		// return(
+		//     <p>there should be something</p>
+		//     )
+	}
 }
 
 export default Feedback;
